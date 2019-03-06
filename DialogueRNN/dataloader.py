5bee1be434223abda04d5e365318e0ee606ad9f8
Soujanya Poria <40890991+soujanyaporia@users.noreply.github.com>
2019-03-07 01:43:58 +0800

    Update dataloader.py

--- DialogueRNN/dataloader.py
+++ DialogueRNN/dataloader.py
@@ -62,4 +62,31 @@ class AVECDataset(Dataset):
     def collate_fn(self, data):
         dat = pd.DataFrame(data)
         return [pad_sequence(dat[i]) if i<4 else pad_sequence(dat[i], True) for i in dat]
+class MELDDataset(Dataset):
 
+    def __init__(self, path, train=True):
+        self.videoIDs, self.videoSpeakers, self.videoLabels, self.videoText,\
+        self.videoAudio, self.videoSentence, self.trainVid,\
+        self.testVid = pickle.load(open(path, 'rb'))
+        '''
+        label index mapping = {'hap':0, 'sad':1, 'neu':2, 'ang':3, 'exc':4, 'fru':5}
+        '''
+        self.keys = [x for x in (self.trainVid if train else self.testVid)]
+
+        self.len = len(self.keys)
+
+    def __getitem__(self, index):
+        vid = self.keys[index]
+        return torch.FloatTensor(self.videoText[vid]),\
+               torch.FloatTensor(self.videoAudio[vid]),\
+               torch.FloatTensor(self.videoSpeakers[vid]),\
+               torch.FloatTensor([1]*len(self.videoLabels[vid])),\
+               torch.LongTensor(self.videoLabels[vid]),\
+               vid
+
+    def __len__(self):
+        return self.len
+
+    def collate_fn(self, data):
+        dat = pd.DataFrame(data)
+        return [pad_sequence(dat[i]) if i<3 else pad_sequence(dat[i], True) if i<5 else dat[i].tolist() for i in dat]
